ccf3b0a1c95c25efe143051ef1458519fb6cfa4d
Timm Friebe <thekid@thekid.de>
2022-03-13 11:49:33 +0100

    QA: Apidocs

--- src/main/php/com/mongodb/Session.class.php
+++ src/main/php/com/mongodb/Session.class.php
@@ -4,10 +4,22 @@ use com\mongodb\io\Protocol;
 use lang\Closeable;
 use util\UUID;
 
+/**
+ * A client session
+ *
+ * @see   https://docs.mongodb.com/manual/reference/server-sessions/
+ * @see   https://docs.mongodb.com/manual/core/read-isolation-consistency-recency/#std-label-sessions
+ */
 class Session implements Closeable {
   private $proto, $id;
   private $closed= false;
-  
+
+  /**
+   * Creates a new session
+   *
+   * @param  com.mongodb.io.Protocol
+   * @param  string|util.UUID $id
+   */
   public function __construct(Protocol $proto, $id) {
     $this->proto= $proto;
     $this->id= $id instanceof UUID ? $id : new UUID($id);
