12a4cdec24305aa410b37cb2746ff688c3134dfc
Timm Friebe <thekid@thekid.de>
2023-12-30 10:15:19 +0100

    Add missing test for Code class

---
+++ src/test/php/com/mongodb/unittest/CodeTest.class.php
@@ -0,0 +1,44 @@
+<?php namespace com\mongodb\unittest;
+
+use com\mongodb\Code;
+use test\{Assert, Test};
+
+class CodeTest {
+  const SOURCE= 'console.log("Test")';
+
+  #[Test]
+  public function can_create() {
+    new Code(self::SOURCE);
+  }
+
+  #[Test]
+  public function source() {
+    Assert::equals(self::SOURCE, (new Code(self::SOURCE))->source());
+  }
+
+  #[Test]
+  public function length() {
+    Assert::equals(strlen(self::SOURCE), (new Code(self::SOURCE))->length());
+  }
+
+  #[Test]
+  public function hash_of() {
+    Assert::equals('JS2892344631', (new Code(self::SOURCE))->hashCode());
+  }
+
+  #[Test]
+  public function string_representation() {
+    Assert::equals('com.mongodb.Code(`console.log("Test")`)', (new Code(self::SOURCE))->toString());
+  }
+
+  #[Test]
+  public function string_cast() {
+    Assert::equals(self::SOURCE, (string)new Code(self::SOURCE));
+  }
+
+  #[Test]
+  public function compare() {
+    Assert::equals(0, (new Code(self::SOURCE))->compareTo(new Code(self::SOURCE)));
+    Assert::equals(1, (new Code(self::SOURCE))->compareTo(new Code('')));
+  }
+}
