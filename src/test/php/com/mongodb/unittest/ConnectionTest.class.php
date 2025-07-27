715bc7718cde0aaa72e66d22d0bfd73064c5cbf5
Timm Friebe <thekid@thekid.de>
2025-07-27 10:42:11 +0200

    Test compressed and uncompressed I/O

--- src/test/php/com/mongodb/unittest/ConnectionTest.class.php
+++ src/test/php/com/mongodb/unittest/ConnectionTest.class.php
@@ -1,5 +1,6 @@
 <?php namespace com\mongodb\unittest;
 
+use com\mongodb\Int64;
 use com\mongodb\io\{BSON, Connection, Compression};
 use peer\ConnectException;
 use test\verify\Runtime;
@@ -15,6 +16,25 @@ class ConnectionTest {
     return [
       pack('VVVV', strlen($payload) + 36, 0, 0, Connection::OP_REPLY),
       pack('VPVV', 0, 0, 0, 1).$payload
+    ];
+  }
+
+  /** Creates an OP_MSG message */
+  private function msg(array $document): array {
+    $payload= $this->bson->sections($document);
+    return [
+      pack('VVVV', strlen($payload) + 21, 0, 0, Connection::OP_MSG),
+      pack('VC', 0, 0).$payload
+    ];
+  }
+
+  /** Creates an OP_COMPRESSED message with an embedded OP_MSG opcode */
+  private function compressed(array $document): array {
+    $payload= pack('VC', 0, 0).$this->bson->sections($document);
+    $compressed= gzcompress($payload);
+    return [
+      pack('VVVV', strlen($compressed) + 25, 0, 0, Connection::OP_COMPRESSED),
+      pack('VVC', Connection::OP_MSG, strlen($payload), 2).$compressed
     ];
   }
 
@@ -89,5 +109,37 @@ class ConnectionTest {
     $c->establish();
 
     Assert::instance(Compression::class, $c->compression);
+  }
+
+  #[Test]
+  public function send_and_receive() {
+    $documents= [['_id' => 'one']];
+    $c= new Connection(new TestingSocket([
+      ...$this->reply(['ok' => 1.0]),
+      ...$this->msg([
+        'cursor' => ['firstBatch' => $documents, 'id' => new Int64(0), 'ns' => 'test.entries'],
+        'ok'     => 1,
+      ]),
+    ]));
+    $c->establish();
+    $reply= $c->send(Connection::OP_MSG, "\x00\x00\x00\x00\x00", ['find' => 'entries', '$db' => 'test']);
+
+    Assert::equals($documents, $reply['body']['cursor']['firstBatch']);
+  }
+
+  #[Test, Runtime(extensions: ['zlib'])]
+  public function send_and_receive_compressed() {
+    $documents= [['_id' => 'one']];
+    $c= new Connection(new TestingSocket([
+      ...$this->reply(['ok' => 1.0, 'compression' => ['zlib']]),
+      ...$this->compressed([
+        'cursor' => ['firstBatch' => $documents, 'id' => new Int64(0), 'ns' => 'test.entries'],
+        'ok'     => 1,
+      ]),
+    ]));
+    $c->establish();
+    $reply= $c->send(Connection::OP_MSG, "\x00\x00\x00\x00\x00", ['find' => 'entries', '$db' => 'test']);
+
+    Assert::equals($documents, $reply['body']['cursor']['firstBatch']);
   }
 }
